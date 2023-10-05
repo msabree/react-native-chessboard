83b34ff9b898de76acda3bb352f449883f846ffa
Makeen Sabree <makeen.sabree@gmail.com>
2023-10-05 11:11:40 -0400

    Delete library specifc code that was moved to a separate repo.

--- src/utils/flipBoard.ts
+++
@@ -1,8 +0,0 @@
-const rotate = (matrix: any[][]) => {
-  return matrix[0].map((_, index) => matrix.map(row => row[index]).reverse());
-};
-
-export const flipBoard = (board: any[][]) => {
-  // Flip twice to get black on bottom;
-  return rotate(rotate(board));
-};
